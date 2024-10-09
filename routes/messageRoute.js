1c4e4bf1a7011995685ffdf644f1e180a748ea7b
novus318 <nizamudheen318@gmail.com>
2024-10-09 12:04:35 +0530

    updates

--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -1,21 +1,26 @@
 import express  from "express";
 import messageModel from "../model/messageModel.js";
-const router=express.Router()
+import dotenv from 'dotenv'
+import axios from "axios";
 
+
+const router=express.Router()
+dotenv.config({ path: '../.env' })
+const ACCESS_TOKEN = process.env.WHATSAPP_TOKEN;
 
 const downloadMedia = async (Id) => {
   const url = `https://graph.facebook.com/v16.0/${Id}`;  // WhatsApp media URL
  try{
    const response = await axios.get(url, {
       headers: {
-          Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`  // Replace with your access token
+          Authorization: `Bearer ${ACCESS_TOKEN}`  // Replace with your access token
       },
   });
   // Convert the binary data to a blob (or buffer)
   if (response.data.url){
     const mediaResponse = await axios.get(response.data.url, {
       headers: {
-        Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}` // Replace with your access token
+        Authorization: `Bearer ${ACCESS_TOKEN}` // Replace with your access token
       },
       responseType: 'stream', // Handle streaming response
     });
@@ -38,90 +43,112 @@ router.post('/webhook', async (req, res) => {
       const { entry } = req.body;
 
       if (entry && entry.length > 0) {
-          const changes = entry[0].changes;
-          if (changes && changes.length > 0) {
-              const messageData = changes[0].value.messages;
+        const changes = entry[0].changes;
+        if (changes && changes.length > 0) {
+            const messageData = changes[0].value.messages;
 
-              if (messageData && messageData.length > 0) {
-                  const message = messageData[0];
-                  let newMessage;
+            if (messageData && messageData.length > 0) {
+                const message = messageData[0];
+                let newMessage;
 
-                  // Common fields for all messages
-                  const senderName = changes[0].value.contacts[0].profile.name;
-                  const senderNumber = message.from;
-                  const timestamp = message.timestamp;
+                // Common fields for all messages
+                const senderName = changes[0].value.contacts[0].profile.name;
+                const senderNumber = message.from;
 
-                  switch (message.type) {
-                      case 'text':
-                          // Handle text message
+                switch (message.type) {
+                    case 'text':
+                        // Handle text message
+                        newMessage = new messageModel({
+                            senderName,
+                            senderNumber,
+                            messageContent: message.text.body,
+                            messageType: 'text'
+                        });
+                        break;
+
+                    case 'reaction':
+                        // Handle reaction message
+                        newMessage = new messageModel({
+                            senderName,
+                            senderNumber,
+                            emoji: message.reaction.emoji,
+                            messageType: 'reaction',
+                            reactedToMessageId: message.reaction.messsage_id
+                        });
+                        break;
+
+                        case 'image':
+                          // Handle image message, download and save as blob
+                          const imageBlob = await downloadMedia(message.image.id);
                           newMessage = new messageModel({
                               senderName,
                               senderNumber,
-                              messageContent: message.text.body,
-                              messageType: 'text'
+                              messageContent: message.image.caption,
+                              messageType: 'image',
+                              mediaBlob: imageBlob,
+                              mediaType: message.image.mime_type,
                           });
                           break;
 
-                      case 'reaction':
-                          // Handle reaction message
+                      case 'sticker':
+                          // Handle sticker message, download and save as blob
+                          const stickerBlob = await downloadMedia(message.sticker.id);
                           newMessage = new messageModel({
                               senderName,
                               senderNumber,
-                              emoji: message.reaction.emoji,
-                              messageType: 'reaction',
-                              reactedToMessageId: message.reaction.messsage_id
+                              messageType: 'sticker',
+                              mediaBlob: stickerBlob,
+                              mediaType: message.sticker.mime_type,
                           });
                           break;
 
-                          case 'image':
-                            // Handle image message, download and save as blob
-                            const imageBlob = await downloadMedia(message.image.id);
+                          case 'audio':
+                            const audioBlob = await downloadMedia(message.audio.id);
                             newMessage = new messageModel({
-                                senderName,
-                                senderNumber,
-                                messageContent: message.image.caption,
-                                messageType: 'image',
-                                mediaBlob: imageBlob,
-                                mediaType: message.image.mime_type,
-                            });
-                            break;
-
-                        case 'sticker':
-                            // Handle sticker message, download and save as blob
-                            const stickerBlob = await downloadMedia(message.sticker.id);
-                            newMessage = new messageModel({
-                                senderName,
-                                senderNumber,
-                                messageType: 'sticker',
-                                mediaBlob: stickerBlob,
-                                mediaType: message.sticker.mime_type,
-                            });
-                            break;
-
-                            case 'audio':
-                              const audioBlob = await downloadMedia(message.audio.id);
-                              newMessage = new messageModel({
-                                senderName,
-                                senderNumber,
-                                messageContent: message.audio.duration,
-                                messageType: 'audio',
-                                mediaBlob: audioBlob,
-                                mediaType: message.audio.mime_type,
-                            });
-                            break;
-                      default:
-                          console.log(`Unknown message type: ${message.type}`);
+                              senderName,
+                              senderNumber,
+                              messageContent: message.audio.duration,
+                              messageType: 'audio',
+                              mediaBlob: audioBlob,
+                              mediaType: message.audio.mime_type,
+                          });
                           break;
-                  }
 
-                  // Save the message to MongoDB
-                  if (newMessage) {
-                      await newMessage.save();
-                      console.log('Message saved:', newMessage);
-                  }
-              }
-          }
-      }
+                          case 'video':
+                            const videoBlob = await downloadMedia(message.video.id);
+                            newMessage = new messageModel({
+                              senderName,
+                              senderNumber,
+                              messageContent: message.video.duration,
+                              messageType: 'video',
+                              mediaBlob: videoBlob,
+                              mediaType: message.video.mime_type,
+                          });
+                          break;
+                         
+                          case 'document':
+                            const documentBlob = await downloadMedia(message.document.id);
+                            newMessage = new messageModel({
+                              senderName,
+                              senderNumber,
+                              messageContent: message.document.filename,
+                              messageType: 'document',
+                              mediaBlob: documentBlob,
+                              mediaType: message.document.mime_type,
+                          });
+                    default:
+                        console.log(`Unknown message type: ${message.type}`);
+                        break;
+                }
+
+                // Save the message to MongoDB
+                if (newMessage) {
+                    await newMessage.save();
+                    console.log('Message saved:', newMessage);
+                }
+            }
+        }
+    }
 
       res.sendStatus(200); // Acknowledge the request
   } catch (error) {
@@ -160,8 +187,16 @@ router.get('/messages', async (req, res) => {
     });
   }
 })
+router.get('/media/:id', async (req, res) => {
+    const media = await messageModel.findById(req.params.id)
+    if (!media) {
+      return res.status(404).json({ error: 'Media not found.' });
+    }
+    res.set('Content-Type', media.mediaType);
+    return res.send(media.mediaBlob);  
+})
 
-obliging-factual-rat.ngrok-free.approuter.delete('/messages/delete', async (req, res) => {
+router.delete('/messages/delete', async (req, res) => {
   const { senderName, senderNumber } = req.body;
 
   try {
